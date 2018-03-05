3ad1834338ab2f1deadf1b829d7b44c3dd9466d7
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 19:13:30 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -11,5 +11,3 @@ def happy_birthday(birthday_kids)
     end
   end
 end
-
-
